2c53fc236ac49e96fc691bbeedf1053a4017c833
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-07-21 13:36:36 +0530

    db issue

--- config/database.js
+++ config/database.js
@@ -7,6 +7,9 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME", "ayna-backend"), //Name of database
       user: env("DATABASE_USERNAME", "postgres"), //Default username
       password: env("DATABASE_PASSWORD", "prince24akshat"), //Password to your PostgreSQL database
+      ssl:{
+        rejectUnauthorized: false
+      }
     },
   },
 });
